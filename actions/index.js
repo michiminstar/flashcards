5ec31f8156fc48e7cebf3adef9e4d26d78d72e6e
Michi Kanda <michi.kanda.0513@gmail.com>
2017-11-05 12:57:44 +0900

    Add actions

---
+++ actions/index.js
@@ -0,0 +1,18 @@
+export const LOAD_DECKS = 'LOAD_DECKS'
+export const ADD_DECK = 'ADD_DECK'
+export const ADD_QUESTION = 'ADD_QUESTION'
+
+export const loadDecks = decks => ({
+  type: LOAD_DECKS,
+  decks
+})
+
+export const addDeck = decks => ({
+  type: ADD_DECK,
+  deck
+})
+
+export const addQuestion = params => ({
+  type: ADD_QUESTION,
+  params,
+})
