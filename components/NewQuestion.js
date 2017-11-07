8d138c099f299e3d0f8ae605715592831db9cad8
Michi Kanda <michi.kanda.0513@gmail.com>
2017-11-07 18:26:09 +0900

    Add the Add New Card feature

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,16 +1,77 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native'
+import { connect } from 'react-redux'
+import { addQuestion } from '../actions'
+import { addCardToDeck } from '../util/api'
+import { Button } from 'react-native-elements'
 
 class NewQuestion extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Add Card'
+    }
+  }
+
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  onSubmit = () =>{
+    const { question, answer } = this.state
+    const {title, questions} = this.props.navigation.state.params
+    const params = {title, questions, question, answer}
+
+    this.props.dispatch(addQuestion(params))
+
+    addCardToDeck({
+      card: { question, answer },
+      title
+    })
+
+    Alert.alert(
+      'Successful', 'Your question has been saved',
+      [
+        {
+          text: 'OK',
+          onPress: () => this.props.navigation.goBack()
+        }
+      ]
+    )
+  }
 
   render() {
+    const { question, anwer } = this.state
+
     return (
       <View>
-        <Text>New Question</Text>
+        <Text>Question</Text>
+        <TextInput
+          onChangeText={(question) => this.setState({ question })}
+          placeholder="Enter Your Question"
+        />
+
+        <Text>Answer</Text>
+        <TextInput
+          onChangeText={(answer) => this.setState({ answer })}
+          placeholder="Enter the Answer"
+        />
+
+        <Button
+          title='Save' style={{marginTop: 10, padding: 20}}
+          backgroundColor='#006DFD'
+          onPress={this.onSubmit}
+        />
       </View>
     )
   }
 
 }
 
-export default NewQuestion
+function mapStateToProps(state) {
+  return {
+    decks: state
+  }
+}
+
+export default connect(mapStateToProps)(NewQuestion)
--- reducers/index.js
+++ reducers/index.js
@@ -13,6 +13,14 @@ function decks(state = {}, action) {
         ...state,
         ...decks
       }
+    case ADD_QUESTION:
+      const { title, questions, question, answer } = action.params
+      const newQuestions = JSON.parse(JSON.stringify(questions)).concat([ { question, answer } ])
+
+      return {
+        ...state,
+        [title]: { ...state[title], questions: newQuestions },
+      }
     default:
       return state
   }
